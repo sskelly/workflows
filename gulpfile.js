fe5a3d2b1de0a5000db187bdee24d7425aea45d2
Scott Skelly <scott@cornmazesamerica.com>
2017-11-26 12:59:28 -0600

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -22,8 +23,10 @@ gulp.task('coffee', function(){
     .pipe(gulp.dest('components/scripts'))
 });
 
+// this combines all the javascript files together into a single file
 gulp.task('js', function(){
-  gulp.src(jsSources)
-    .pipe(concat('script.js'))
+  gulp.src(jsSources) // uses the array of files above to combine everything
+    .pipe(concat('script.js'))//makes a new file with this name
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
