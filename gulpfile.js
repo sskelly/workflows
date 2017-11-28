1815707ddda0d53a2b73f39417e781b23cc9ef4a
Scott Skelly <scott@cornmazesamerica.com>
2017-11-28 06:21:17 -0600

    Minify HTML

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -77,7 +78,7 @@ gulp.task('watch', function(){ // this will run everytime the file changes
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('_components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(jsonSources, ['js']);
 });
 
@@ -89,7 +90,9 @@ gulp.task('connect', function(){
 });
 
 gulp.task('html', function(){
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
 
